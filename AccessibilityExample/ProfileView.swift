8556f772ab240f92a896288b8ae66d448566f189
Norbert Grover <norbert298@gmail.com>
2023-11-04 19:59:44 -0700

    Added a period

--- AccessibilityExample/ProfileView.swift
+++ AccessibilityExample/ProfileView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Here's your profile.")
+        Text("Here's your profile...")
             .accessibilityLabel("Here is your profile")
             .navigationTitle("Profile")
     }
